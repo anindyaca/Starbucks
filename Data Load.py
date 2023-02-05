7a71dcba63a2e4e03f919b6a0a8787c73c8795f2
anindyachakraborty <anindya.catalyst@gmail.com>
2023-02-05 11:10:30 +0530

    Release 2

--- Data Load.py
+++ Data Load.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import numpy as np
+import math
+import json
+from sklearn.preprocessing import LabelEncoder
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import warnings
 
+from preprocessing_helper import encode_person_id, encode_offer_id, convert_data_type, encode_channel
+import progressbar
+warnings.filterwarnings(action='once')
 
 #loading the JSON files -- Orient/lines params definition --> "https://pandas.pydata.org/docs/reference/api/pandas.read_json.html"
 portfolio = pd.read_json('Data/portfolio.json', orient='records', lines=True)
